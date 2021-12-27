086557c69009960987590d6b88fbdcf78086a319
flugsvampe <svante@godoy.tech>
2021-12-27 11:44:35 +0100

    masken kan nu gå

--- masken.py
+++ masken.py
@@ -1,20 +1,61 @@
 from microbit import *
-import music
+import time
 
-# pins
-Buzzer_pin = pin0
-CrashSensor_pin = pin1
-
-# set up crash sensor
-CrashSensor_pin.set_pull(CrashSensor_pin.PULL_UP)
+mask_x = 0
+mask_y = 2
+direction_x = 1
+direction_y = 0
 
 while True:
-    # if crash sensor is pressed, play a note, if not stop the music
-    if CrashSensor_pin.read_digital() == 1:
-        display.set_pixel(0, 0, 0)
-        music.stop()
 
-    else:
-        display.set_pixel(0, 0, 9)
-        music.play("c4:3")
+    a_presses = button_a.get_presses()
+    b_presses = button_b.get_presses()
+
+    display.set_pixel(mask_x, mask_y, 0)
+    mask_x = mask_x + direction_x
+    mask_y = mask_y + direction_y
+
+    if (mask_x >= 5):
+        mask_x = 0
+
+    if (mask_x < 0):
+        mask_x = 4
+
+    if (mask_y >= 5):
+        mask_y = 0
+
+    if (mask_y < 0):
+        mask_y = 4
+
+    display.set_pixel(mask_x, mask_y, 9)
+
+    time.sleep(0.5)
+
+    if (a_presses != button_a.get_presses()):
+        if (direction_y == 0):
+            if (direction_x == 1):
+                direction_y = -1
+            else:
+                direction_y = 1
+            direction_x = 0
+        else:
+            if (direction_y == 1):
+                direction_x = 1
+            else:
+                direction_x = -1
+            direction_y = 0
+
+    if (b_presses != button_b.get_presses()):
+        if (direction_y == 0):
+            if (direction_x == 1):
+                direction_y = 1
+            else:
+                direction_y = -1
+            direction_x = 0
+        else:
+            if (direction_y == 1):
+                direction_x = -1
+            else:
+                direction_x = 1
+            direction_y = 0
 
